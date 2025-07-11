975ec0e0d43333185e6c503cde6f150e4c08aa63
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2025-07-11 21:42:54 +0530

    feat: integrated avail da rollup blueprint with jmt, nomt storage options

--- examples/demo-rollup/src/zk.rs
+++ examples/demo-rollup/src/zk.rs
@@ -42,6 +42,26 @@ pub fn celestia_risc0_host_args() -> Arc<&'static [u8]> {
     )
 }
 
+/// Returns the risc0 host arguments for a rollup with mock da. This is the code that is zk-proven by the rollup
+pub fn avail_risc0_host_args() -> Arc<&'static [u8]> {
+    // Don't try to read the elf file if we're not building the risc0 guest!
+    if should_skip_guest_build() {
+        return Arc::new(vec![].leak());
+    }
+
+    Arc::new(
+        std::fs::read(risc0::ROLLUP_PATH)
+            .unwrap_or_else(|e| {
+                panic!(
+                    "Could not read guest elf file from `{}`. {}",
+                    risc0::ROLLUP_PATH,
+                    e
+                )
+            })
+            .leak(),
+    )
+}
+
 fn should_skip_guest_build() -> bool {
     match std::env::var("SKIP_GUEST_BUILD")
         .as_ref()
